3777f316841a97e862298b32009022483c4fe8eb
masakisaitou <sight.masaki@gmail.com>
2026-06-05 17:14:56 +0900

    feat: add MVP seeders

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's MVP learning master data.
+     */
+    public function run(): void
+    {
+        $this->call([
+            CategorySeeder::class,
+            TagSeeder::class,
+            QuestionSeeder::class,
+        ]);
+    }
+}
