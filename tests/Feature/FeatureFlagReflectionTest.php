8030d64fa108f47726f44ab3a1c10d83d296bab5
masakisaitou <sight.masaki@gmail.com>
2026-07-09 11:48:52 +0900

    test: verify worker feature flag reload

--- tests/Feature/FeatureFlagReflectionTest.php
+++ tests/Feature/FeatureFlagReflectionTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Http\Request;
 use Illuminate\Support\Env;
+use Illuminate\Support\Facades\Artisan;
 use Tests\TestCase;
 
 class FeatureFlagReflectionTest extends TestCase
@@ -32,6 +33,56 @@ class FeatureFlagReflectionTest extends TestCase
             $this->assertFalse((bool) config('features.speech_pronunciation_assessment_enabled'));
             $this->assertFalse((bool) config('features.speech_fluency_assessment_enabled'));
         });
+    }
+
+    public function test_running_application_reads_new_feature_flags_after_restart(): void
+    {
+        $this->withFeatureFlagEnvironment('false', 'false', function (): void {
+            Env::enablePutenv();
+            $this->refreshApplication();
+
+            $this->assertFalse((bool) config('features.speech_pronunciation_assessment_enabled'));
+            $this->assertFalse((bool) config('features.speech_fluency_assessment_enabled'));
+
+            $this->setEnvironmentValue('SPEECH_PRONUNCIATION_ASSESSMENT_ENABLED', 'true');
+            $this->setEnvironmentValue('SPEECH_FLUENCY_ASSESSMENT_ENABLED', 'true');
+
+            // A long-running process keeps its boot-time config until it is restarted.
+            $this->assertFalse((bool) config('features.speech_pronunciation_assessment_enabled'));
+            $this->assertFalse((bool) config('features.speech_fluency_assessment_enabled'));
+
+            $this->refreshApplication();
+
+            $this->assertTrue((bool) config('features.speech_pronunciation_assessment_enabled'));
+            $this->assertTrue((bool) config('features.speech_fluency_assessment_enabled'));
+        });
+    }
+
+    public function test_config_cache_regeneration_uses_current_feature_flag_environment(): void
+    {
+        try {
+            $this->withFeatureFlagEnvironment('true', 'true', function (): void {
+                Artisan::call('config:clear');
+                Artisan::call('config:cache');
+
+                $cachedConfig = require $this->app->getCachedConfigPath();
+
+                $this->assertTrue($cachedConfig['features']['speech_pronunciation_assessment_enabled']);
+                $this->assertTrue($cachedConfig['features']['speech_fluency_assessment_enabled']);
+            });
+
+            $this->withFeatureFlagEnvironment('false', 'false', function (): void {
+                Artisan::call('config:clear');
+                Artisan::call('config:cache');
+
+                $cachedConfig = require $this->app->getCachedConfigPath();
+
+                $this->assertFalse($cachedConfig['features']['speech_pronunciation_assessment_enabled']);
+                $this->assertFalse($cachedConfig['features']['speech_fluency_assessment_enabled']);
+            });
+        } finally {
+            Artisan::call('config:clear');
+        }
     }
 
     public function test_inertia_shared_features_are_boolean_cast_from_config(): void
